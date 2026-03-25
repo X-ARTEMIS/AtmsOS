65b61e1321510729b6367c6591c556c65e780cac
Ghoul <CursedGhoulBlox@gmail.com>
2026-03-25 13:08:54 -0400

    Update ew.c

--- ew.c
+++ ew.c
@@ -4,43 +4,34 @@ int main() {
     double firstNumber;
     double secondNumber;
     double answer;
-    double operator;
+    char operator;
 
     printf("Enter your first number\n");
-    scanf("%lf",&firstNumber);
+    scanf(" %lf",&firstNumber);
     printf("Enter your operator\n");
-    scanf("%c", operator);
+    scanf(" %c", &operator);
     printf("Enter your second number\n");
-    scanf("%lf", secondNumber);
+    scanf(" %lf", &secondNumber);
 
-    // th you can't use switches on chars in C?
-    // switches have such strange progression
-    // in C, they only work on integers
-    // in C++ they work on chars too
-    // in C# they work on strings
-
-    if (operator == '*') {
-        answer = firstNumber * secondNumber;
+    switch(operator) {
+        case '*':
+            answer = firstNumber * secondNumber;
+            break;
+        case '-':
+            answer = firstNumber - secondNumber;
+            break;
+        case '+':
+            answer = firstNumber + secondNumber;
+            break;
+        case '/':
+            answer = firstNumber * secondNumber;
+            break;
+        default:
+            printf("Invalid something\n");
+            return 1;
     }
 
-    else if (operator == '/') {
-        answer = firstNumber / secondNumber;
-    }
-
-    else if (operator == '+') {
-        answer = firstNumber + secondNumber;
-    }
-
-    else if (operator == '-') {
-        answer = firstNumber + secondNumber;
-    }
-
-    else {
-        printf("Invalid something\n");
-        return 1;
-    }
-
-    printf("Your answer is %d", answer);
+    printf("Your answer is %lf\n", answer);
     printf("************************");
 
     // I hate C C++ is far superior. In C++ we trust
